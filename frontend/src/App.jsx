0e375b390cc3c464839605b502eab1c466bc1c7a
Renzo <renzo11demarco@gmail.com>
2025-09-17 23:54:09 -0300

    add scrollToTop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ReportCreate from "./pages/ReportCreate";
 import ReportDetail from "./pages/ReportDetail";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
 
   return (
     <Router>
-        <div className="flex-1 p-6">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/new" element={<ReportCreate />} />
-            <Route path="/report/:n" element={<ReportDetail />} />
-          </Routes>
-        </div>
+      <ScrollToTop />
+      <div className="flex-1 p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<ReportCreate />} />
+          <Route path="/report/:n" element={<ReportDetail />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
